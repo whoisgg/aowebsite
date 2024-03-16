88b30c95b7a70f257a2de173b7f6776dd00cc93f
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-03-15 21:17:01 -0300

    first commit

---
+++ components/Pages/Profile.tsx
@@ -0,0 +1,6 @@
+import styles from "../../styles/section.module.scss";
+
+const Profile = () => {
+  return <div className={styles.profile}>Your profile content here</div>;
+};
+export default Profile;
---
+++ components/Pages/Section1.tsx
@@ -0,0 +1,4 @@
+const Section1 = () => {
+  return <div> Section content here</div>;
+};
+export default Section1;
