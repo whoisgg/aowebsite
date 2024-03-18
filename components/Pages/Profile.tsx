e9607846d2b5cb96c91572bf97b1be8a228626d0
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-03-18 12:47:57 -0300

    sn links

--- components/Pages/Profile.tsx
+++ components/Pages/Profile.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "../../styles/section.module.scss";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Profile = () => {
   return (
@@ -22,9 +24,18 @@ const Profile = () => {
 
       {/* Social Networks Bar */}
       <div className="flex justify-center space-x-4 pt-5">
-        <FaFacebook className="text-3xl cursor-pointer hover:text-blue-600" />
-        <FaTwitter className="text-3xl cursor-pointer hover:text-blue-400" />
-        <FaInstagram className="text-3xl cursor-pointer hover:text-pink-600" />
+        <Link
+          href="https://www.instagram.com/dra.antonietaortegamunoz/"
+          passHref
+        >
+          <FaInstagram className="text-3xl cursor-pointer hover:text-pink-600" />
+        </Link>
+        <Link href="https://wa.me/56984644659" passHref>
+          <FaWhatsapp className="text-3xl cursor-pointer hover:text-green-400" />
+        </Link>
+        <Link href="https://twitter.com/" passHref>
+          <FaSquareXTwitter className="text-3xl cursor-pointer hover:invert" />
+        </Link>
       </div>
     </div>
   );
