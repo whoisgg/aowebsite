021ec0b5ac3e1144725955fea416e4676cbb6ebe
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-03-19 20:11:02 -0300

    test

--- components/Pages/Section3.tsx
+++ components/Pages/Section3.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../../styles/section.module.scss";
 
 const Section3 = () => {
-  return <div className={`${styles.profile2}`}>Section 4</div>;
+  return <div className={`${styles.profile2}`}>SectioN 4</div>;
 };
 
 export default Section3;
