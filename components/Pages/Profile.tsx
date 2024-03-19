548411e8b97a5c5183567d4e249cf666e1bd5589
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-03-19 20:44:47 -0300

    lighthouse arialabel

--- components/Pages/Profile.tsx
+++ components/Pages/Profile.tsx
@@ -29,14 +29,21 @@ const Profile = () => {
         <Link
           href="https://www.instagram.com/dra.antonietaortegamunoz/"
           passHref
+          aria-label="instagram"
         >
           <FaInstagram className="text-3xl cursor-pointer hover:text-pink-600" />
         </Link>
         <Link href="https://wa.me/56984644659" passHref>
-          <FaWhatsapp className="text-3xl cursor-pointer hover:text-green-400" />
+          <FaWhatsapp
+            className="text-3xl cursor-pointer hover:text-green-400"
+            aria-label="whatsapp"
+          />
         </Link>
         <Link href="https://twitter.com/" passHref>
-          <FaSquareXTwitter className="text-3xl cursor-pointer hover:invert" />
+          <FaSquareXTwitter
+            className="text-3xl cursor-pointer hover:invert"
+            aria-label="twitter"
+          />
         </Link>
       </div>
     </div>
